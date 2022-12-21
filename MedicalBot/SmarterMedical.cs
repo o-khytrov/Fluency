362e071ee1fb93430dd6516d6c2da3007f67da12
Oleksandr Khytrov <oleksandr.khytrov@justanswer.com>
2022-12-21 11:01:03 +0200

    ++

---
+++ MedicalBot/SmarterMedical.cs
@@ -0,0 +1,16 @@
+﻿using Engine;
+
+namespace MedicalBot;
+
+public class SmarterMedical : GenericBot<MedicalBotContext>
+{
+    public string Sympathy { get; set; } = string.Empty;
+
+    public string QuestionType { get; set; }
+
+    public SmarterMedical()
+    {
+        Gambit("MEDICAL_START")
+            .WithOutput($"Welcome! How can I help you with your {QuestionType} question?");
+    }
+}
