f18cba69ea7ff34766ab55cad4f98e727314edb1
Oleksandr Khytrov <oleksandr.khytrov@justanswer.com>
2022-12-29 13:35:34 +0200

    legal bot

---
+++ Bots/LegalBot/LegalBot.cs
@@ -0,0 +1,28 @@
+﻿using FluentConversation.Engine;
+using FluentConversation.Engine.Tokenization;
+
+namespace LegalBot;
+
+public class LegalBot : Bot<LegalConversationContext>
+{
+    private static readonly Dictionary<string, string> CategoryTopicMap = new()
+    {
+        { "traffic-law", "traffic law" }
+    };
+
+    public LegalBot()
+    {
+        R("LEGAL_START")
+            .When(LegalStartCondition).Output((c) => $"Welcome! How can I help with your {c.Topic} question?").Keep();
+    }
+
+    private bool LegalStartCondition(LegalConversationContext conversation, BotInput botInput)
+    {
+        if (conversation.Category is not null && CategoryTopicMap.ContainsKey(conversation.Category))
+        {
+            conversation.Topic = CategoryTopicMap[conversation.Category];
+        }
+
+        return true;
+    }
+}
---
+++ Bots/LegalBot/LegalConversationContext.cs
@@ -0,0 +1,14 @@
+namespace LegalBot;
+
+public class LegalConversationContext
+{
+    public string? Specialty { get; set; }
+
+    public string? CategoryPreUrl { get; set; }
+
+    public string? Category { get; set; }
+
+    public string? OriginalCategory { get; set; }
+
+    public string? Topic { get; set; }
+}
